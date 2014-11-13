8181599d88e18e9380e2f82823dc304c88832c72
donatello <donatello@donatello.(none)>
2014-11-13 00:16:39 -0500

    Initializing repository

---
+++ src/SimpleServer.py
@@ -0,0 +1,66 @@
+import socket
+from threading import Thread
+
+class SimpleClient:
+  def __init__(self, host="localhost", port=12345): #pr2mm1.csail.mit.edu
+    self.host = host
+    self.port = port
+    self.message_received = None
+
+  def get_message(self):
+    self.s = socket.socket()         # Create a socket object
+    self.s.connect((self.host, self.port))
+    self.message_received = self.s.recv(1024)
+    self.s.close                     # Close the socket when done
+    return self.message_received
+  
+  
+class SimpleServer:
+  def __init__(self, port=12346, threading=False):
+    self.s = socket.socket()
+    self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+    self.s.bind(("", port))
+    self.s.listen(5)
+    self.msg = "Not Ready"
+    self.stop_broadcast = False
+    self.threading = threading
+
+    if self.threading:
+      self.t = Thread(target=self.threaded_broadcast)
+      self.t.start()
+
+  def update_broadcast(self, msg):
+    self.msg = msg
+
+  def threaded_broadcast(self):
+    while not self.stop_broadcast:
+      c, addr = self.s.accept()
+      c.send(self.msg)
+      c.close()
+      print "send!"
+    print "done send"
+  
+  def broadcast(self, msg):
+    c, addr = self.s.accept()
+    c.send(msg)
+    c.close()
+
+if __name__=="__main__":
+
+  server_type = raw_input("For Broadcaster input B for Listener  input L\n")
+  if server_type == "B":
+    broadcaster = SimpleServer()
+    for i in range(5):
+      msg = raw_input("Enter text you want to broadcast: ")
+      broadcaster.broadcast(msg)
+      #broadcaster.broadcast("hello! %d " % i)
+      #broadcaster.msg = "Broadcasting message now  %d " % i
+    #broadcaster.stop_broadcast = True
+  else:
+    listener = SimpleClient()
+    while True:
+      raw_input("get client message?")
+      msg = listener.get_message()
+      print"listening message is: %s " %  msg
+
+
---
+++ src/pr2_communication.py
@@ -0,0 +1,93 @@
+import rospy
+from SimpleServer import SimpleServer, SimpleClient
+from argparse import ArgumentParser
+
+class interface:
+  START, PICKING, WAITING_TO_PLACE, PLACING= range(4)
+  def __init__(self, init_state=None, debug=False):
+      self.server  = SimpleServer(12345)
+      self.client = SimpleClient(host="localhost",port=12346) #10.68.0.171
+      
+      if init_state == None:
+          self.state = self.START
+      else: 
+          self.state = init_state
+
+      self.event_loop()
+      
+  def event_loop(self):
+      while True:
+          rospy.loginfo("starting event loop!  current state = %d" % self.state)
+
+          if self.state == self.START:
+              self.state = self.PICKING
+              self.pick_up()
+              self.state = self.WAITING_TO_PLACE
+          
+          if self.state == self.WAITING_TO_PLACE:
+              self.send_msg_to_turtle("pr2 ready to place can")
+              self.wait_until_msg_is("turtle in place position")
+              self.state = self.PLACING
+          
+          if self.state == self.PLACING:
+              self.place()
+              self.send_msg_to_turtle("pr2 placed object")
+              self.wait_until_msg_is("turtle left pr2")
+              self.state = self.PICKING
+
+          if self.state == self.PICKING:
+              rospy.loginfo("rotating")
+              self.pick_up()
+              self.state = self.WAITING_TO_PLACE
+
+  def wait_until_msg_is(self,correct_msg):
+      rospy.loginfo("waiting to receive following msg from turtle: %s"\
+      % correct_msg)
+      msg = self.receive_msg_from_turtle()
+      while msg != correct_msg:
+          msg = self.receive_msg_from_turtle()
+              
+  def send_msg_to_turtle(self, msg):
+      rospy.loginfo( "sending message: %s " % msg)
+      self.server.broadcast(msg)
+      rospy.loginfo( "message sent: %s " % msg)
+  
+  def receive_msg_from_turtle(self):
+      msg_received = False
+      while not msg_received:
+          try:
+              msg = self.client.get_message()
+              msg_received = True
+              rospy.loginfo("message received is: %s " % msg)
+          except:
+              rospy.sleep(1)
+      return msg
+
+  def pick_up(self):
+      rospy.loginfo("picking up object")
+      rospy.sleep(1) # dummy action 
+
+  def place(self):
+      rospy.loginfo("placing can")
+      rospy.sleep(1) # dummy action
+
+def awesome_parse_arguments():
+    parser = ArgumentParser("Select initial state")
+    parser.add_argument("--wait", action="store_true",
+        help="wait for msg then place")
+    parser.add_argument("--place", action="store_true",
+        help="in place position")
+
+    args = parser.parse_args()
+    init_state = 0
+    if args.wait:
+        init_state=  2
+    if args.place:
+        init_state = 3
+    return init_state
+if __name__=="__main__":
+  rospy.init_node("turtle_pr2_friends")
+  init_state = awesome_parse_arguments()
+  interface(init_state)
+
+
